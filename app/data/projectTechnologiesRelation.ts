11ae00ed8fdfab059d040462a1e1b0dd4317b06c
Mauro Leal <wolfhackd@gmail.com>
2026-05-26 09:17:00 -0300

    feat: add technologies to projects

--- app/data/projectTechnologiesRelation.ts
+++ app/data/projectTechnologiesRelation.ts
@@ -5,6 +5,10 @@ export const projectTechnologiesRelation = [
       '780e07fb-78c5-4877-9590-0f12900c560f', // TypeScript
       '817db415-d4a8-46f7-8e28-8db199d9c192', // Node.js
       '7d9f2ae2-899d-4932-b050-c54e73fa7f62', // Tailwind CSS
+      'f09da993-a9b2-422d-834a-f55341305436', // React
+      '9dbbec03-4713-4b26-aaee-00bc91b7e904', // Shadcn
+      '88e9b43f-d59b-4bb6-8883-a5c5fd0036b9', // Zod
+      '94899b62-44fe-49dc-986c-cf92a26dcad3', // Drizzle ORM 
     ]
   },
   {
@@ -12,6 +16,9 @@ export const projectTechnologiesRelation = [
     techIds: [
       '93371d9d-c291-46c4-9b91-f6464987471a', // JavaScript
       '817db415-d4a8-46f7-8e28-8db199d9c192', // Node.js
+      'f09da993-a9b2-422d-834a-f55341305436', // React
+      '7d9f2ae2-899d-4932-b050-c54e73fa7f62', // Tailwind CSS
+
     ]
   },
   {
@@ -19,6 +26,10 @@ export const projectTechnologiesRelation = [
     techIds: [
       '780e07fb-78c5-4877-9590-0f12900c560f', // TypeScript
       'a592cf34-0b32-4cb2-8e2b-1257eb0eb44e', // Next.js
+      'f09da993-a9b2-422d-834a-f55341305436', // React
+      '9dbbec03-4713-4b26-aaee-00bc91b7e904', // Shadcn
+      '88e9b43f-d59b-4bb6-8883-a5c5fd0036b9', // Zod
+      '94899b62-44fe-49dc-986c-cf92a26dcad3', // Drizzle ORM 
     ]
   }
 ];
